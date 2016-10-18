e5f213dc50a44aff0242069de9d5980638a4695f
ChristophWalter <c.walter-95@gmx.de>
2016-10-18 11:48:01 +0200

    get and save notification dummy send email

---
+++ Modules/SendMail.js
@@ -0,0 +1,31 @@
+/**
+ * Created by CWalt on 18.10.2016.
+ */
+//install http://nilhcem.github.io/FakeSMTP/
+
+var nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport('smtp://localhost');
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: '"TaskManager" <localhost>', // sender address
+    to: '', // list of receivers
+    subject: 'CRITICAL!', // Subject line
+    text: '' // plaintext body
+};
+
+exports.send = function(notification, text){
+    mailOptions.to=notification.email;
+    mailOptions.text=
+        "Achtung, die Temperatur ist über "+
+        notification.value+"°C";
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            return console.log(error);
+        }
+        console.log('Message sent: '+info.response);
+    });
+};
--- renderer.js
+++ renderer.js
@@ -2,3 +2,8 @@
  * Created by traub on 10.10.2016.
  */
 
+var mail = require('./Modules/SendMail');
+var tempNotification = require('./Modules/SaveTempNotification');
+
+tempNotification.save({email:"critical@taskmanager.de",value:35});
+mail.send(tempNotification.get());
