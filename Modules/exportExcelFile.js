f17857b3618f3a270400b4452510b7c79c76fb18
sedioben <o.sedi@yahoo.de>
2016-10-19 09:35:03 +0200

    export requirement hotfix + switched language to german

--- Modules/exportExcelFile.js
+++ Modules/exportExcelFile.js
@@ -13,47 +13,53 @@ var exportExcelFile = function () {
     processData.getAllProcesses(function (data) {
         var content = data;
 
-        var options = {}; //Defining Options of SaveAsDialog
-        options.title = " Save as ExcelFile";
-        options.buttonLabel = "Save";
-        options.filters = [
-            {name: "Excel-Workbook(*.xlsx)", extensions: ["xlsx"]},
-            {name: "All Files", extensions: ["*"]}
-        ];
+        if (content) {
+            var options = {}; //Defining Options of SaveAsDialog
+            options.title = " Exceldatei Speichern";
+            options.buttonLabel = "Speichern";
+            options.filters = [
+                {name: "Excel-Arbeitsmappe(*.xlsx)", extensions: ["xlsx"]},
+                {name: "Alle Dateien", extensions: ["*"]}
+            ];
 
 
-        dialog.showSaveDialog(options, function (fileName) {
+            dialog.showSaveDialog(options, function (fileName) {
 
-            var workbook = new Excel.Workbook();
-            var sheet = workbook.addWorksheet("Statistics");
-            //Determines header and content rows by content object
-            var header = Object.keys(content[0]);
-            var getRows = function () {
-                var rows = [];
-                for (var i = 0; i < content.length; i++) {
-                    //Map function which gets the values of objects (ECMAScript 6)
-                    rows.push(Object.keys(content[i]).map(key => content[i][key]));
+                var workbook = new Excel.Workbook();
+                var sheet = workbook.addWorksheet("Statistics");
+                //Determines header and content rows by content object
+                var header = Object.keys(content[0]);
+                var getRows = function () {
+                    var rows = [];
+                    for (var i = 0; i < content.length; i++) {
+                        //Map function which gets the values of objects (ECMAScript 6)
+                        rows.push(Object.keys(content[i]).map(key => content[i][key]));
+                    }
+                    return rows;
+                };
+
+                sheet.addRow(header);
+                sheet.addRows(getRows());
+
+                //Styling
+                sheet.getRow(1).border = {
+                    bottom: {style: "thick"}
+                };
+
+
+                if (fileName === undefined) {
+                    alert("Die Datei wurde nicht gespeichert");
+                    console.log("Der Dateipfad wurde nicht bestimmt");
                 }
-                return rows;
-            };
 
-            sheet.addRow(header);
-            sheet.addRows(getRows());
+                workbook.xlsx.writeFile(fileName);
+                alert("Die Datei wurde erfolgreich gespeichert");
+            });
 
-            //Styling
-            sheet.getRow(1).border = {
-                bottom: {style: "thick"}
-            };
+        } else {
+            alert("Es bestehen keine laufende Prozesse");
+        }
 
-
-            if( fileName === undefined){
-                alert("You didn't save the file");
-                console.log("file directory wasn't defined");
-            }
-
-            workbook.xlsx.writeFile(fileName);
-            alert("The file has been successfully saved");
-        });
     });
 
 };
