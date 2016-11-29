c6fb08077881e1507bfd1cc7ea51802b08e96dfe
traub <sascha.traub@hpe.com>
2016-11-29 11:12:46 +0100

    - created test cases for cpu temp created test cases for process data

--- Test_modules/NodeTests.js
+++ Test_modules/NodeTests.js
@@ -1,3 +1,23 @@
 /**
  * Created by traub on 29.11.2016.
  */
+var cpuData = require('../Modules/CpuData');
+var processData = require('../Modules/ProcessData')
+var test = require('unit.js');
+
+
+describe('Check if Hardwaer data is available', function() {
+    it('testing cpu data retrival', function(done) {
+        cpuData.getcpuTempArray(function (data) {
+            test.array(data)
+            done();
+        })
+    });
+
+    it('testing process data retrival', function(done) {
+        processData.getAllProcesses(function (data) {
+            test.array(data).isNot([]);
+            done();
+        });
+    });
+});
