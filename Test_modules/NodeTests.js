516e2e50affe6d38af0f3bfecf6b589c14db1d4c
marcjako <jakob.marcel@web.de>
2016-11-29 13:09:16 +0100

    added DB tests

--- Test_modules/NodeTests.js
+++ Test_modules/NodeTests.js
@@ -2,8 +2,9 @@
  * Created by traub on 29.11.2016.
  */
 var cpuData = require('../Modules/CpuData');
-var processData = require('../Modules/ProcessData')
+var processData = require('../Modules/ProcessData');
 var test = require('unit.js');
+var saveTempNotification = require('../Modules/SaveTempNotification');
 
 
 describe('Check if Hardwaer data is available', function() {
@@ -17,6 +18,33 @@ describe('Check if Hardwaer data is available', function() {
     it('testing process data retrival', function(done) {
         processData.getAllProcesses(function (data) {
             test.array(data).isNot([]);
+            done();
+        });
+    });
+});
+
+describe('Check DB actions', function() {
+    it('testing if max temp is saved', function(done){
+        //test notification
+        var notification = {
+            email: "test@test.com",
+            value: 50
+        };
+
+        saveTempNotification.saveMaxTempObject(notification, function (data) {
+            test
+                .string(data)
+                .is('success');
+            done();
+        });
+    });
+
+    it('testing max temp retrival', function(done) {
+        saveTempNotification.getMaxTempObject(function (data) {
+            test
+                .object(data)
+                .hasProperty('email')
+                .hasProperty('value');
             done();
         });
     });
