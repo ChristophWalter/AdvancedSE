86503bd91c0e0bb025a0497d8cc5c6fac75f90fc
miwehrs <m.wehrstein@gmx.de>
2016-12-06 12:58:20 +0100

    Minor fixes to renderer.js

--- renderer.js
+++ renderer.js
@@ -2,14 +2,7 @@
  * Created by traub on 10.10.2016.
  */
 var updater = require('./Modules/updater.js');
-var mail = require('./Modules/SendMail');
-var exportExcelFile = require('./Modules/exportExcelFile');
 var domManipulater = require('./Modules/domManipulater');
-var tempNotification = require('./Modules/SaveTempNotification');
-
-var jQuery = require('jquery');
-var processList = "";
-
 
 setInterval(updater.data,1000);
 
